5661d1a1fc7eca90e3f78ec08c5b94091217472c
Luccas Adriano <devluccas@DESKTOP-3C6SL42.localdomain>
2022-09-03 15:38:04 -0300

    feat: update decorater EventPattern

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common'
-import { MessagePattern, Payload } from '@nestjs/microservices'
+import { EventPattern, Payload } from '@nestjs/microservices'
 import { AppService } from './app.service'
 import { IPhone, IEmail } from './types/notification.interface'
 
@@ -7,12 +7,12 @@ import { IPhone, IEmail } from './types/notification.interface'
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern('notification-email')
+  @EventPattern('notification-email')
   async sendEmail(@Payload() data: IEmail): Promise<void> {
     await this.appService.sendEmail(Number(data.id), data.email, data.name)
   }
 
-  @MessagePattern('notification-sms')
+  @EventPattern('notification-sms')
   async sendSMS(@Payload() data: IPhone): Promise<void> {
     await this.appService.sendSMS(data.id, data.phone, data.name)
   }
